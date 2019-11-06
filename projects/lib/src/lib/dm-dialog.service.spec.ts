014ab87801cb775fc9877a970fad91e389a141cf
Dmitry Mukhin <dimanomail@gmail.com>
2019-11-06 17:08:13 +0400

    build: update configs. remove karma

--- projects/lib/src/lib/dm-dialog.service.spec.ts
+++ projects/lib/src/lib/dm-dialog.service.spec.ts
@@ -1,4 +1,4 @@
-import { createServiceFactory, SpectatorService } from '@ngneat/spectator';
+import { createServiceFactory, SpectatorService } from '@ngneat/spectator/jest';
 
 import { DmDialogService } from './dm-dialog.service';
 
